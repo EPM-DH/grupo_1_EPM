3a006eaab6d27691f9a28744191a8452e8185a7b
abejotav1 <abejotav1@hotmail.com>
2021-11-16 11:56:04 -0600

    Se crea arbol
    Plantillas de HTML

---
+++ app.js
@@ -0,0 +1,25 @@
+const express =require('express');
+const path = require('path')
+const port = 3500;
+
+const app = express();
+const publicPath = path.resolve(__dirname, './public');
+app.use (express.static(publicPath));
+
+app.listen(port,() => console.log(`Servidor corriendo en puerto ${port}`));
+
+app.get('/',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
+app.get('/register',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+})
+app.get('/login',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+})
+app.get('/login',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/buycar.html'));
+})
+app.get('/login',(req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/details.html'));
+})
