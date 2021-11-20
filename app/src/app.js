6276688e5476bc8e084e02b0c78711291c6a7fad
RicardoZ <litosiderito@gmail.com>
2021-11-19 18:56:40 -0600

    Revert "[VIEWS] Actualización de views y estándar de CSS"
    This reverts commit 7e8ee26956231d2ac600f7b249b9c3d85d96bc5c.

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const bodyParser = require('body-parser'); /* Bodyparser lo usaré para hacer el handling de los POST */
-const urlencodedParser = bodyParser.urlencoded ({ extended: false }); /* url encoder lo uso para hacer strings los datos de los post */
 const app = express();
 const path = require('path');
 const port = 3500;
@@ -22,28 +20,10 @@ app.get('/login', (req,res)=> {
     res.sendFile(path.resolve(__dirname,'./views/login.html'));
 });
 
-<<<<<<< Updated upstream:app/src/app.js
 app.get('/buy', (req, res)=> {
     res.sendFile(path.resolve(__dirname,'./views/buycar.html'));
-=======
-app.get('/shoppingcart', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/shoppingcart.html'));
->>>>>>> Stashed changes:app/app.js
 });
 
 app.get('/details', (req, res)=> {
     res.sendFile(path.resolve(__dirname,'./views/details.html'));
 });
-app.get('/searchresult', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/searchresult.html'));
-});
-app.post('/searchresult', urlencodedParser, function(req, res) {
-    console.log(req.body);
-    console.log("Funcionó");
-    res.redirect('/searchresult');
-})
-app.post('/register', urlencodedParser, function(req, res) {
-    console.log(req.body);
-    console.log("Funcionó el registro");
-    res.redirect('/');
-})
