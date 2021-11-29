ee5fa6ced9371e03d1865238e009d2b147fafa89
JuanMaLR <A01206568@itesm.mx>
2021-11-29 16:59:15 -0600

    Añadiendo EJS al proyecto

--- app/app.js
+++ app/app.js
@@ -5,33 +5,44 @@ const app = express();
 const path = require('path');
 const port = 3500;
 const publicPath = path.resolve(__dirname, './public');
+
 app.use (express.static(publicPath));
+app.set("view engine", "ejs")
+
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
+
 app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+    res.render('home');
 }); 
+
 app.get('/register', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+    res.render('register');
 });
+
 app.get('/login', (req,res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+    res.render('login');
 });
+
 app.get('/shoppingcart', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/shoppingcart.html'));
+    res.render('shoppingcart');
 });
+
 app.get('/details', (req, res)=> {
     res.sendFile(path.resolve(__dirname,'./views/details.html'));
 });
+
 app.get('/searchresult', (req, res)=> {
     res.sendFile(path.resolve(__dirname,'./views/searchresult.html'));
 });
+
 app.post('/searchresult', urlencodedParser, function(req, res) {
     console.log(req.body);
     console.log("Funcionó");
     res.redirect('/searchresult');
-})
+});
+
 app.post('/register', urlencodedParser, function(req, res) {
     console.log(req.body);
     console.log("Funcionó el registro");
     res.redirect('/');
-})
+});
