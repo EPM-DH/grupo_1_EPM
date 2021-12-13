7049b8af6ca188add029e714271771366b52f233
JuanMaLR <A01206568@itesm.mx>
2021-12-12 19:56:12 -0600

    Implementing EJS on cart page

--- app/app.js
+++ app/app.js
@@ -7,20 +7,17 @@ const mainRouter = require('./routes/main');
 const userRouter = require('./routes/user');
 const searchRouter = require('./routes/search');
 const productRouter = require('./routes/product');
+const shoppingRouter = require('./routes/shopping');
 
 app.use (express.static(publicPath));
 app.use('/', mainRouter);
 app.use('/user', userRouter);
 app.use('/searchresult', searchRouter); //Consider changing into main router
 app.use('/product', productRouter);
+app.use('/shoppingCart', shoppingRouter);
 app.set("view engine", "ejs");
 
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
-
-//Create route and controller to orderProcessing
-app.get('/shoppingcart', (req, res)=> {
-    res.render('shoppingcart');
-});
 
 app.use((req, res, next) => {
     res.status(404).render('not-found');
