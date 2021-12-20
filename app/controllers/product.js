4a8cc2a6c44f20ef57aa27437ff0701674e71139
JuanMaLR <A01206568@itesm.mx>
2021-12-20 16:41:57 -0600

    Implementing POST in products

--- app/controllers/product.js
+++ app/controllers/product.js
@@ -9,9 +9,44 @@ const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 const categoriesFilePath = path.join(__dirname, '../data/categories.json');
 const categories = JSON.parse(fs.readFileSync(categoriesFilePath, 'utf-8'));
 
+//Consider adding rating field to products JSON 
+
 const productController = {
 	retrieveCreate: (req, res) => {
-		res.render('./products/createProduct');
+		res.render('./products/createProduct', {categories});
+	},
+	create: (req, res) => {
+		let characteristics = [];
+		//To get the value of each characteristic
+		for (const [key, value] of Object.entries(req.body)) {
+			if(key.includes('characteristic')) {
+				characteristics.push(value);
+			}
+		};
+
+		//Create new product from form data
+		let newProduct = {
+			id: products[products.length - 1].id + 1,
+			name: req.body.name,
+			price: req.body.price,
+			categories: req.body.categories,
+			shortDescription: req.body.shortDescription,
+			longDescription: req.body.longDescription,
+			characteristics: characteristics,
+			identifier: req.body.identifier, //Validate if identifier is unique
+			vendidos: 0,
+			toBuy: 0,
+			image: req.file.filename, 
+			carouselImages: [req.file.filename, req.file.filename, req.file.filename], //Update in following sprints
+		};
+
+		//Add new product
+		products.push(newProduct);
+
+		//Write the new product to the JSON file
+		fs.writeFileSync(productsFilePath, JSON.stringify(products));
+		
+		res.redirect('/'); //Products don't update until the page is reloaded 
 	},
     retrieveEdit: (req, res) => {
 		let id = req.params.id;
@@ -27,9 +62,9 @@ const productController = {
 			if(key.includes('characteristic')) {
 				characteristics.push(value);
 			}
-		}
+		};
 
-		//Create new product from form data
+		//Create updated product from form data
 		let newProduct = {
 			id: id,
 			name: req.body.name,
