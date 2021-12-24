281110903ad52538cad760130c1df693ee7a539a
abejotav1 <abejotav1@hotmail.com>
2021-12-23 20:17:46 -0600

    Update Spring 4
    Actualizando app.js

--- app/app.js
+++ app/app.js
@@ -3,26 +3,26 @@ const app = express();
 const path = require('path');
 const port = 3500;
 const publicPath = path.resolve(__dirname, './public');
+const methodOverride = require('method-override');
 const mainRouter = require('./routes/main');
-const registerRouter = require('./routes/register');
+const userRouter = require('./routes/user');
 const searchRouter = require('./routes/search');
+const productRouter = require('./routes/product');
+const shoppingRouter = require('./routes/shopping');
 
-app.use (express.static(publicPath));
+app.use(express.static(publicPath));
+app.use(methodOverride('_method')); // For PUT and DELETE methods
+app.use(express.urlencoded({ extended: false })); //To be able to retrieve the forms data in the req element
+app.use(express.json()); //To be able to retrieve the forms data in the req element
 app.use('/', mainRouter);
-app.use('/register', registerRouter);
-app.use('/searchresult', searchRouter);
-app.set("view engine", "ejs")
+app.use('/user', userRouter);
+app.use('/searchresult', searchRouter); //Consider changing into main router
+app.use('/product', productRouter);
+app.use('/shoppingCart', shoppingRouter);
+app.set("view engine", "ejs");
 
 app.listen(port, () => console.log(`Servidor corriendo en puerto ${port}`));
 
-app.get('/login', (req,res)=> {
-    res.render('login');
-});
-
-app.get('/shoppingcart', (req, res)=> {
-    res.render('shoppingcart');
-});
-
-app.get('/details', (req, res)=> {
-    res.sendFile(path.resolve(__dirname,'./views/details.html'));
+app.use((req, res, next) => {
+    res.status(404).render('not-found');
 });
